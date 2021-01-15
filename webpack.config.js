75e3467193d224b676af6fccc0746531b1d35b73
Mateusz Kajsztura <mkajsztura@davinci-studio.eu>
2021-01-15 12:48:59 +0100

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      publicPath: "",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  entry: {
+    app: ["./src/main.js", "./src/scss/app.scss"],
+  },
+  output: {
+    filename: "[name].js",
+    path: resolve(__dirname, "dist"),
+    publicPath: "",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {},
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              sourceMap: true,
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "assets",
+            publicPath: "./assets",
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: 9000,
+  },
+};
